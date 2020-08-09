dc756ba93097c8e40b086f1f13d0a860ceea5374
keyflight <olivier@andev.nl>
2020-08-09 14:00:43 +0200

    fix: 🐛 make /usr/bin/zsh default shell

--- Dockerfile
+++ Dockerfile
@@ -99,6 +99,8 @@ EXPOSE 8080
 USER coder
 WORKDIR /home/coder
 
+ENV SHELL /usr/bin/zsh
+
 # This is where volume mount happens
 
 ENTRYPOINT /srv/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,9 @@
 
 sudo service docker start
 
-dumb-init fixuid -q /usr/bin/code-server --bind-addr 0.0.0.0:8080 .
+if [ ! -f /tmp/foo.txt ]; then
+    echo "Oh My Zsh not found. Installing now..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+dumb-init fixuid -q /usr/bin/code-server --bind-addr 0.0.0.0:8080 --auth none .
