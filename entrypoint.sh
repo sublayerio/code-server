44f393d5d41ab59e55ddbd7f1f397e2ed2369928
keyflight <olivier@andev.nl>
2020-08-09 14:02:14 +0200

    fix: 🐛 check if .oh-my-zsh dir exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 sudo service docker start
 
-if [ ! -f /tmp/foo.txt ]; then
+if [ ! -d "/home/coder/.oh-my-zsh" ]; then
     echo "Oh My Zsh not found. Installing now..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
